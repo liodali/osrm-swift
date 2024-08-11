95c7554fb63e57c02585a05942b0d487bdc92ef5
liodali <16631886+liodali@users.noreply.github.com>
2024-08-11 12:06:32 +0200

    add unit test

--- Package.swift
+++ Package.swift
@@ -1,12 +1,11 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "osrm-swift",
     platforms: [.macOS(.v10_13),
-                 .iOS(.v12)],
+                 .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +14,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
-        .package(url: "https://github.com/raphaelmor/Polyline.git", from: "5.0.2")
+        .package(url: "https://github.com/raphaelmor/Polyline.git", from: "5.0.2"),
+        //.package(url: "https://github.com/WeTransfer/Mocker.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/JohnSundell/Files.git", from: "4.2.0"),
+        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.1.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -24,11 +26,25 @@ let package = Package(
             name: "osrm-swift",
             dependencies: [
                 .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "Polyline", package: "Polyline")
+                .product(name: "Polyline", package: "Polyline"),
+                
+            ],
+            resources: [
+                .process("resources/en.json"),
+                .process("resources/es.json"),
+                .process("resources/ar.json"),
+                .process("resources/de.json"),
             ]),
         .testTarget(
             name: "osrm-swiftTests",
-            dependencies: ["osrm-swift"]),
+            dependencies: [
+                "osrm-swift",
+                //"Mocker",
+                "Files",
+                .product(name: "OHHTTPStubs", package: "OHHTTPStubs"),
+                .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs")
+            ],
+            resources: [.process("res_tests/example1.json")]),
         
     ]
 )
